f323d33b3daac1bff55f37673e52d6e16dcab28b
Jakub Słowiak <slowiakjakub@outlook.com>
2023-07-04 01:28:25 +0200

    Added some Countries

--- HotelListing.API/Data/Country.cs
+++ HotelListing.API/Data/Country.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
+
+        public virtual IList<Hotel> Hotels { get; set; }
     }
 }
